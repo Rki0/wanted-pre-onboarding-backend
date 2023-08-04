38f9b1734e668f53e8f27d34661bc47ff6eedd86
Rki0 <pky00823@gmail.com>
2023-08-04 14:49:38 +0900

    feat: 로그인 기능 구현
    입력받은 email이 DB에 존재하는지,
    입력받은 password가 복호화된 password와 일치하는지 확인합니다.
    두 조건을 만족하면 JWT를 생성하고 json에 담아 반환합니다.

--- routes/user.js
+++ routes/user.js
@@ -16,4 +16,15 @@ router.post(
   userController.signUpUser
 );
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("유효한 이메일 주소를 입력해주세요."),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("최소 8자리 이상 입력해주세요."),
+  ],
+  userController.loginUser
+);
+
 module.exports = router;
