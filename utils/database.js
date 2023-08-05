7620e391c39a1806574b8b57c6f763c2d5cbd93f
Rki0 <pky00823@gmail.com>
2023-08-06 00:56:10 +0900

    feat: 배포 환경을 위한 DB 연결 .env 설정

--- utils/database.js
+++ utils/database.js
@@ -4,9 +4,14 @@ dotenv.config();
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("wanted", "root", process.env.DB_PASSWORD, {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
